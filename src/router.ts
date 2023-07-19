5d15423f746d5846e1d61b2cecafd558853c659b
Ashly LaMarr <surlymae@gmail.com>
2023-07-19 11:33:08 -0600

    Add content api skeleton routes

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,26 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/content", (req, res) => {
-  res.json({ message: "content" });
+  //get all of user's content
 });
 
+router.get("/content/:id", () => {
+  //get user's content by id
+});
+
+router.get("/content/:type", () => {
+  //get user's content by content type
+});
+
+router.post("/content", () => {
+  //create content for this user
+});
+
+router.put("/content/:id", () => {
+  //edit this content
+});
+
+router.delete("/content/:id", () => {
+  //delete this content
+});
 export default router;
